c24839567eba2565b72aa6717f460d42edc98417
abhay <65488882+iimabhi@users.noreply.github.com>
2022-04-16 08:43:01 +0530

    Add files via upload

---
+++ special.py
@@ -0,0 +1,17 @@
+from tkinter import CENTER
+from turtle import *
+
+
+
+color('red')
+bgcolor('white')
+begin_fill()
+pensize(10)
+left(50)
+forward(130)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(130)
+end_fill()
+
